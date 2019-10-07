b70e1f50a8423e1aff12f5809589bd8297ad65cb
marlondck <15050029+marlondck@users.noreply.github.com>
2019-10-06 21:50:08 -0400

    pegando posicoes dos elementos

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', 'prettier'],
   rules: {
@@ -22,14 +22,15 @@ module.exports = {
     'react/jsx-filename-extension': [
       'warn',
       {
-        extensions: ['.jsx', '.js']
-      }
+        extensions: ['.jsx', '.js'],
+      },
     ],
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
     'no-param-reassign': 'off',
     'no-console': ['error', { allow: ['tron'] }],
+    'no-useless-return': 'off',
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
-  }
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 };
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,11 +3,11 @@ import { useDrag, useDrop } from 'react-dnd';
 
 import { Container, Label } from './styles';
 
-export default function Card({ data }) {
+export default function Card({ data, index }) {
   const ref = useRef();
 
   const [{ isDragging }, dragRef] = useDrag({
-    item: { type: 'CARD', id: data.id },
+    item: { type: 'CARD', index },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     }),
@@ -16,8 +16,34 @@ export default function Card({ data }) {
   const [, dropRef] = useDrop({
     accept: 'CARD',
     hover(item, monitor) {
-      console.log(item.id);
-      console.log(data.id);
+      // console.log(item.index, index);
+      const draggetIndex = item.index;
+      const targetIndex = index;
+
+      if (draggetIndex === targetIndex) {
+        return;
+      }
+      // pega posicao do elemento
+      const targetSize = ref.current.getBoundingClientRect();
+      // console.log(targetSize);
+      // pega a metade da posicao do elemento
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+      //  console.log(targetCenter);
+      // valor do item arrastado
+      const draggetOffset = monitor.getClientOffset();
+      // console.log(draggetOffset);
+      // pegando as posicoes dos dois cards a serem alterados
+      const draggedTop = draggetOffset.y - targetSize.top;
+      // console.log(draggedTop, targetCenter);
+      // se arrastar um card e ainda estiver antes da metade e/ou inicio do outro card não faca nada
+      if (draggetIndex < targetIndex && draggedTop < targetCenter) {
+        return;
+      }
+      // console.log('passou da metade do card');
+      // se arrastar um card de baixo pra cima e ainda não chegou na metade da posicao do card, não faca nda
+      if (draggetIndex < targetIndex && draggedTop > targetCenter) {
+        return;
+      }
     },
   });
 
